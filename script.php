b94e97809d4079d3ac27b0d010a611e3f7d4b75b
MadLiy <134729527+MadLiy@users.noreply.github.com>
2023-08-18 19:40:59 +0300

    Script.php

--- script.php
+++ script.php
@@ -52,15 +52,36 @@ $xml = '
 
 $xmlObject = simplexml_load_string($xml);
 
-$fLigne = $xmlObject -> fLigne;
+$fLigne = $xmlObject - >fLigne;
 
-$produit_pocleunik = $fLigne -> PRODUIT_POCLEUNIK;
-$produit_ref = $fLigne -> PRODUIT_REF;
-$refciale_arcleunik = $fLigne -> REFCIALE_ARCLEUNIK;
-/* ... */
+$produit_pocleunik = $fLigne - >PRODUIT_POCLEUNIK;
+$produit_ref = $fLigne - >PRODUIT_REF;
+$refciale_arcleunik = $fLigne - >REFCIALE_ARCLEUNIK;
+$refciale_refart = $fLigne - >REFCIALE_REFART;
+$refciale_refcat = $fLigne - >REFCIALE_REFCAT;
+$potrad_desi = $fLigne - >POTRAD_DESI;
+$refciale_ctva = $fLigne - >REFCIALE_CTVA;
+$fictech_memocat = $fLigne - >FICTECH_MEMOCAT;
+$fictech_memonet = $fLigne - >FICTECH_MEMONET;
+$produit_marque = $fLigne - >PRODUIT_MARQUE;
+$produit_clep01 = $fLigne - >PRODUIT_CLEP01;
+$produit_clep02 = $fLigne - >PRODUIT_CLEP02;
+$produit_clep03 = $fLigne - >PRODUIT_CLEP03;
+$produit_clep04 = $fLigne - >PRODUIT_CLEP04;
+$produit_clep06 = $fLigne - >PRODUIT_CLEP06;
+$produit_clep07 = $fLigne - >PRODUIT_CLEP07;
+$produit_gcoloris = $fLigne - >PRODUIT_GCOLORIS;
+$produit_gtaille = $fLigne - >PRODUIT_GTAILLE;
+$produit_clep12 = $fLigne - >PRODUIT_CLEP12;
+$refciale_ficheina = $fLigne - >REFCIALE_FICHEINA;
+$refciale_modte = $fLigne - >REFCIALE_MODTE;
+$produit_modte = $fLigne - >PRODUIT_MODTE;
+$article_poids = $fLigne - >ARTICLE_POIDS;
+$article_hnormel = $fLigne - >ARTICLE_HNORMEL;
+$article_categ = $fLigne - >ARTICLE_CATEG;
 
-$sql = "INSERT INTO produits (produit_pocleunik, produit_ref, refciale_arcleunik)
-        VALUES ('$produit_pocleunik', '$produit_ref', '$refciale_arcleunik')";
+$sql = "INSERT INTO produits (produit_pocleunik, produit_ref, refciale_arcleunik, refciale_refart,refciale_refcat,potrad_desi,refciale_ctva, fictech_memocat,fictech_memonet,produit_marque,produit_clep01,produit_clep02,produit_clep03,produit_clep04,produit_clep06,produit_clep07,produit_gcoloris,produit_gtaille,produit_clep12,refciale_ficheina,refciale_modte,produit_modte,article_poids,article_hnormel,article_categ)
+        VALUES ('$produit_pocleunik', '$produit_ref', '$refciale_arcleunik','refciale_refart','refciale_refcat','potrad_desi','refciale_ctva','fictech_memocat','fictech_memonet','produit_marque','produit_clep01','produit_clep02','produit_clep03','produit_clep04','produit_clep06','produit_clep07',produit_gcoloris,'produit_gtaille','produit_clep12','refciale_ficheina','refciale_modte','produit_modte','article_poids','article_hnormel','article_categ')";
 
 if (mysqli_query($conn, $sql)) {
     echo "Données insérées avec succès.";
@@ -75,13 +96,13 @@ $xml = new MagicParser();
 $xml->open('catalogue.xml'); 
 
 foreach ($xml->catalogue->produit as $produit) {
-    $name = $conn->real_escape_string($product->name);
-    $price = (float) $product->price;
-    $description = $conn->real_escape_string($product->description);
-    $category = $conn->real_escape_string($product->category);
+    $name = $conn->real_escape_string($produit->name);
+    $prix = (float) $produit->prix;
+    $description = $conn->real_escape_string($produit->description);
+    $categorie = $conn->real_escape_string($produit->categorie);
 
-    $sql = "INSERT INTO catalogue_data (product_name, product_price, product_description, product_category)
-            VALUES ('$name', $price, '$description', '$category')";
+    $sql = "INSERT INTO catalogue_data (produit_name, produit_prix, produit_description, produit_categorie)
+            VALUES ('$name', $prix, '$description', '$categorie')";
 
     if ($conn->query($sql) !== TRUE) {
         echo "Error: " . $sql . "<br>" . $conn->error;
@@ -91,5 +112,4 @@ foreach ($xml->catalogue->produit as $produit) {
 $conn->close();
 
 echo "Données insérées avec succès!";
-
 ?>
